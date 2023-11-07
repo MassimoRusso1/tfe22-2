6f29e9d69f6baa581ed105b918ef02226067f1eb
Masimo Russo <122785115+MassimoRusso1@users.noreply.github.com>
2023-11-07 09:57:54 +0100

    solution-003

--- exercise-003/vectorint.hpp
+++ exercise-003/vectorint.hpp
@@ -1,14 +1,21 @@
 #ifndef MY_INTEGER_VECTOR_HPP
 #define MY_INTEGER_VECTOR_HPP
 
-class VectorInt
-{
-   public:
+#include <cstddef>
+
+class VectorInt {
+public:
     VectorInt(int size);
     ~VectorInt();
-    
-   private:
+    int& at( size_t pos );
+    size_t size() const;
+    void resize( size_t count );
+    void push_back( const int& value );
+
+private:
     int m_size;
     int* mp_Data;
+    int m_null{0};
 };
+
 #endif
